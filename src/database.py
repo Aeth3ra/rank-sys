73cbf1c4274941e2f1e038e0f772c6baaba1c105
Aethera <50434287+Aeth3ra@users.noreply.github.com>
2026-01-12 23:24:39 +1100

    Move conn parameter position

--- src/database.py
+++ src/database.py
@@ -1,9 +1,10 @@
 import sqlite3
+from config import Config
 
-def addGame(game: str, conn: sqlite3.Connection):
+def addGame(conn: sqlite3.Connection, game: str):
     conn.execute("INSERT OR IGNORE INTO games VALUES (?)", (game,))
 
-def addSeason(game: str, num: int, conn: sqlite3.Connection):
+def addSeason(conn: sqlite3.Connection, game: str, num: int):
     addGame(game, conn)
     try:
         conn.execute("INSERT INTO seasons (season_num, game_name) VALUES (?, ?)",
@@ -11,5 +12,17 @@ def addSeason(game: str, num: int, conn: sqlite3.Connection):
     except sqlite3.IntegrityError:
         print(f"{game} Season {num} already exists!")
 
-def addPlayer(player: str, conn: sqlite3.Connection):
+def addPlayer(conn: sqlite3.Connection, player: str):
     conn.execute("INSERT OR IGNORE INTO players VALUES (?)", (player,))
+
+def getSeasonId(conn: sqlite3.Connection, game: str, num: int) -> int:
+    cursor = conn.cursor()
+    sql = """--sql
+        SELECT season_id FROM seasons 
+        WHERE upper(game_name) = upper(?) 
+        AND season_num = ?;"""
+    cursor.execute(sql, (game, num))
+    return cursor.fetchone()[0]
+
+def newRating(conn: sqlite3.Connection, season_id: int, player: str, config: Config = Config()):
+    pass
